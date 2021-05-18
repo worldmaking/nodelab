a89be14f7eb00493e05caa8587695df629613e70
Graham Wakefield <grrrwaaa@gmail.com>
2021-05-18 10:22:33 -0400

    first

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const path = require("path")
+const fs = require('fs');
+const https = require('https');
+const { Server } = require('ws');
 const express = require("express")
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+const wss = new Server({ app });
+
+wss.on('connection', (socket) => {
+	console.log('Client connected');
+	socket.send("hi there");
+	socket.on('close', () => console.log('Client disconnected'));
+});
 
 console.log("ok")
 
