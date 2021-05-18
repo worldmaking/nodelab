05acef0d12d78ae983079f1cd4c1d9294fed883f
Graham Wakefield <grrrwaaa@gmail.com>
2021-05-18 10:25:49 -0400

    first

--- server.js
+++ server.js
@@ -9,14 +9,15 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const wss = new Server({ app });
+// const wss = new Server({ app });
 
-wss.on('connection', (socket) => {
-	console.log('Client connected');
-	socket.send("hi there");
-	socket.on('close', () => console.log('Client disconnected'));
-});
+// wss.on('connection', (socket) => {
+// 	console.log('Client connected');
+// 	socket.send("hi there");
+// 	socket.on('close', () => console.log('Client disconnected'));
+// });
 
-console.log("ok")
+
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+console.log("ok")
