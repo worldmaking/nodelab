5d4bcc50245f9a80f2e940a3dbf5d2f34e861f8d
Douglas Gregory <dmgregory+git@gmail.com>
2021-11-18 11:27:37 -0500

    merge as function, not class #4

--- public/merge.js
+++ public/merge.js
@@ -12,25 +12,27 @@ const Automerge = require('automerge');
  * }
  */
 
+/**
+ * Call this function to intialize automerge, and get an object
+ * back that you can use as an interface for transactions.
+ * @param {object} scene initial state of the scene document
+ * @returns a merge object containing functions {handleSyncMessage}
+ */
+function setupMerge(scene) {
+    let syncState = window.Automerge.initSyncState()
 
-class Merger {
+    // make a local automerge doc from our scene
+    const doc1 = window.Automerge.from(scene);
+    const actorID = window.Automerge.getActorId(doc1).toString();
+    // pass new changes to here
 
-    doc1;
-    backends;
+    let backends= {};
+    backends['doc1'] = doc1;
 
-    constructor (scene) {
-        syncState = window.Automerge.initSyncState()
 
-        // make a local automerge doc from our scene
-        let doc1 = window.Automerge.from(scene);
-        const actorID = window.Automerge.getActorId(doc1).toString()
-        // pass new changes to here
-    
-        let backends= {}
-        backends['doc1'] = doc1      
-    }
+    let merger = {};
 
-    handleSyncMessage (message) {
+    merger.handleSyncMessage = function(message) {
         remoteSyncMessage = new Uint8Array(Array.from(message))
         backends.doc1 = Automerge.clone(backends.doc1)
         const [nextBackend, nextSyncState, patch] = Automerge.receiveSyncMessage(
@@ -44,35 +46,10 @@ class Merger {
     
         console.log('adding a new node');
     }
-}
-
-
-const merger = new Merger(initialScene);
-
-
-
-merger.handleSyncMessage(message);
-
-
-
-
-
-
-
-
-
-
-function setupMerge(scene) {
-    let doc1;
-    let backends;
-
-
-
-    let merger = {};
-
-    merger.handleSyncMessage = function(message) {
-        //
-    }
 
     return merger;
 }
+
+try {
+    module.exports = exports = { setupMerge };
+} catch (e) {}
